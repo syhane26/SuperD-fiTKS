623564b5c7ef1b6f45b2e0a5dc5b940c048a57d7
kdaichii <daichi6754712@gmail.com>
2025-12-03 17:14:41 +0100

    création de l'index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Concours Photo</title>
+</head>
+<body>
+
+    <h1>Concours Photo</h1>
+
+    <a href="/index.html">Retour à l’accueil</a>
+
+</body>
+</html>
